84d1dd10c5acd986dc5c4fc3bfc0b89d4cfb7156
bumble.zhou <bumble.zhou@gmail.com>
2017-07-05 22:56:35 +0800

    load remote data and store in local raw files

--- app/src/androidTest/java/qsh/com/animalantiepidemic/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/qsh/com/animalantiepidemic/ExampleInstrumentedTest.java
@@ -4,8 +4,16 @@ import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
+import com.google.gson.Gson;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.PortUnreachableException;
+
+import qsh.com.animalantiepidemic.models.AnimalTypeModel;
 
 import static org.junit.Assert.*;
 
@@ -22,5 +30,30 @@ public class ExampleInstrumentedTest {
         Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("qsh.com.animalantiepidemic", appContext.getPackageName());
+    }
+
+    public String loadResourceFileContent(int fileId) {
+        String result = null;
+        try {
+            Context appContext = InstrumentationRegistry.getTargetContext();
+            InputStream is = appContext.getResources().openRawResource(fileId);
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            result = new String(buffer, "UTF-8");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+        return result;
+    }
+
+    @Test
+    public void load_json_from_raw_file_and_parse_to_json(){
+        String fileContent = loadResourceFileContent(R.raw.animal_types);
+        Gson gson = new Gson();
+        AnimalTypeModel[] animalTypeModels = gson.fromJson(fileContent, AnimalTypeModel[].class);
+        System.out.print("共获取" + animalTypeModels.length + "条记录。");
     }
 }
--- app/src/test/java/qsh/com/animalantiepidemic/ExampleUnitTest.java
+++ app/src/test/java/qsh/com/animalantiepidemic/ExampleUnitTest.java
@@ -1,8 +1,34 @@
 package qsh.com.animalantiepidemic;
 
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Locale;
+
+import qsh.com.animalantiepidemic.models.AnimalTypeModel;
+import qsh.com.animalantiepidemic.models.EarNumberModel;
+import qsh.com.animalantiepidemic.models.FarmerModel;
+import qsh.com.animalantiepidemic.models.UserModel;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -13,5 +39,152 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void load_user_data_from_remote_server_and_store_as_local_json_file() {
+        final String url = "http://www.klmyqsh.com/wx/api/PlatformUser/LoadAllUsers";
+
+        try (CloseableHttpClient httpClient = HttpClientBuilder.create().build()) {
+            HttpPost request = new HttpPost(url);
+            StringEntity params = new StringEntity("");
+            request.addHeader("content-type", "application/json");
+            request.setEntity(params);
+            HttpResponse result = httpClient.execute(request);
+            String json = EntityUtils.toString(result.getEntity(), "UTF-8");
+
+            com.google.gson.Gson gson = new com.google.gson.Gson();
+            UserModel[] userModels = gson.fromJson(json, UserModel[].class);
+
+            System.out.println("共获取" + userModels.length + "条记录。");
+            System.out.println("准备存入文件users.json");
+
+            //write converted json data to a file named "users.json"
+            FileWriter writer = new FileWriter("users.json");
+            writer.write(json);
+            writer.close();
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @Test
+    public void load_farmer_data_from_remote_server_and_store_as_local_json_file() {
+        final String url = "http://www.klmyqsh.com/wx/api/Farmer/LoadAllFarmers";
+
+        try (CloseableHttpClient httpClient = HttpClientBuilder.create().build()) {
+            HttpPost request = new HttpPost(url);
+            StringEntity params = new StringEntity("");
+            request.addHeader("content-type", "application/json");
+            request.setEntity(params);
+            HttpResponse result = httpClient.execute(request);
+            String json = EntityUtils.toString(result.getEntity(), "UTF-8");
+
+            com.google.gson.Gson gson = new com.google.gson.Gson();
+            FarmerModel[] farmerModels = gson.fromJson(json, FarmerModel[].class);
+
+            System.out.println("共获取" + farmerModels.length + "条记录。");
+            System.out.println("准备存入文件farmers.json");
+
+            //write converted json data to a file named "farmers.json"
+            FileWriter writer = new FileWriter("farmers.json");
+            writer.write(json);
+            writer.close();
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @Test
+    public void load_animal_type_data_from_remote_server_and_store_as_local_json_file() {
+        final String url = "http://www.klmyqsh.com/wx/api/AnimalType/LoadAllAnimalTypes";
+
+        try (CloseableHttpClient httpClient = HttpClientBuilder.create().build()) {
+            HttpPost request = new HttpPost(url);
+            StringEntity params = new StringEntity("");
+            request.addHeader("content-type", "application/json");
+            request.setEntity(params);
+            HttpResponse result = httpClient.execute(request);
+            String json = EntityUtils.toString(result.getEntity(), "UTF-8");
+
+            com.google.gson.Gson gson = new com.google.gson.Gson();
+            AnimalTypeModel[] animalTypeModels = gson.fromJson(json, AnimalTypeModel[].class);
+
+            System.out.println("共获取" + animalTypeModels.length + "条记录。");
+            System.out.println("准备存入文件animal_types.json");
+
+            //write converted json data to a file named "animal_types.json"
+            FileWriter writer = new FileWriter("animal_types.json");
+            writer.write(json);
+            writer.close();
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public class DateDeserializer implements JsonDeserializer<Date> {
+
+        private static final String TAG = "DateDeserializer";
+        private final String[] DATE_FORMATS = new String[]{
+                "yyyy-MM-dd'T'HH:mm:ss.SSSSSSZ",
+                "yyyy-MM-dd'T'HH:mm:ssZ",
+                "MMM d, yyyy hh:mm:ss a",// Aug 29, 2016 09:11:08 AM
+                "MMM d, yyyy HH:mm:ss",//Aug 29, 2016 16:11:08
+        };
+
+        @Override
+        public Date deserialize(JsonElement jsonElement, Type typeOF, JsonDeserializationContext context) throws JsonParseException {
+            //先判断是否下面的格式“/Date(1488815571000)/”
+            if(jsonElement.getAsString().contains("/Date(")) {
+                String elementVlue = jsonElement.getAsString().replace("/Date(", "").replace(")/", "");
+                return new Date(Long.valueOf(elementVlue));
+            }
+            else {
+                //否则，就是常见的几种预定格式
+                for (String format : DATE_FORMATS) {
+
+                    try {
+                        return new SimpleDateFormat(format, Locale.US).parse(jsonElement.getAsString());
+                    } catch (ParseException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            throw new JsonParseException("Unparseable date: \"" + jsonElement.getAsString() + "\". Supported formats: " + Arrays.toString(DATE_FORMATS));
+        }
+    }
+
+    @Test
+    public void load_ear_number_data_from_remote_server_and_store_as_local_json_file() {
+        final String url = "http://www.klmyqsh.com/wx/api/ShortUrl/LoadAllEarNumbers";
+
+        try (CloseableHttpClient httpClient = HttpClientBuilder.create().build()) {
+            HttpPost request = new HttpPost(url);
+            StringEntity params = new StringEntity("");
+            request.addHeader("content-type", "application/json");
+            request.setEntity(params);
+            HttpResponse result = httpClient.execute(request);
+            String json = EntityUtils.toString(result.getEntity(), "UTF-8");
+
+            GsonBuilder gsonBuilder = new GsonBuilder();
+            gsonBuilder.registerTypeAdapter(Date.class, new DateDeserializer());
+
+            com.google.gson.Gson gson = gsonBuilder.create();
+            EarNumberModel[] earNumberModels = gson.fromJson(json, EarNumberModel[].class);
+
+            System.out.println("共获取" + earNumberModels.length + "条记录。");
+            System.out.println("准备存入文件ear_numbers.json");
+
+            //write converted json data to a file named "ear_numbers.json"
+            FileWriter writer = new FileWriter("ear_numbers.json");
+            writer.write(json);
+            writer.close();
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
 }
