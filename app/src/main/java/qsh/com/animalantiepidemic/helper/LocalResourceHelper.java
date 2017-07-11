768cb37c6da2574ecf5fc5888c0b1a7be1570330
bumble.zhou <bumble.zhou@gmail.com>
2017-07-11 17:42:24 +0800

    完成添加畜主功能。

--- app/src/main/java/qsh/com/animalantiepidemic/helper/LocalResourceHelper.java
+++ app/src/main/java/qsh/com/animalantiepidemic/helper/LocalResourceHelper.java
@@ -3,8 +3,16 @@ package qsh.com.animalantiepidemic.helper;
 import android.content.Context;
 import android.util.Log;
 
+import com.google.gson.Gson;
+
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import qsh.com.animalantiepidemic.R;
+import qsh.com.animalantiepidemic.models.FarmerModel;
 
 /**
  * Created by JackZhou on 07/07/2017.
@@ -28,5 +36,4 @@ public class LocalResourceHelper {
         }
         return result;
     }
-
 }
